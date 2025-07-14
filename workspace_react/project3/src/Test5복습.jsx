ba00ec893350863a002a2ef7776ffad89b4ac5fd
ne-aki <geunhyeong930@gmail.com>
2025-07-14 22:29:26 +0900

    6587

--- workspace_react/project3/src/Test5복습.jsx
+++ workspace_react/project3/src/Test5복습.jsx
@@ -15,13 +15,19 @@ const Test5복습 = () => {
       <h2>나이 : {man.age}</h2>
       <h2>주소 : {man.addr}</h2>
       <button type="button" onClick={() => {
-        man.name = '홍길동';
-        setMan(man);
+        const copyMan = {...man};
+        copyMan.name = '홍길동';
+        setMan(copyMan);
       }}>이름을 홍길동으로 변경</button>
-      <button type="button">나이를 30으로 변경</button>
-      <button type="button">주소를 서울시로 변경</button>
+      <button type="button" onClick={() => {
+        const copyMan = {...man, age : 30}
+        setMan(copyMan)
+      }}>나이를 30으로 변경</button>
+      <button type="button" onClick={() => {
+        const copyMan = {...man, addr : '서울시'};
+        setMan(copyMan)
+      }}>주소를 서울시로 변경</button>
     </div>
   )
 }
-
 export default Test5복습
