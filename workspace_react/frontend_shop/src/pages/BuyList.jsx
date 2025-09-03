bdf549ba1e68b0665a0ccd8763d20998fe1fff7e
ne-aki <geunhyeong930@gmail.com>
2025-09-03 15:13:22 +0900

    구매이력조회 화면 구현

--- workspace_react/frontend_shop/src/pages/BuyList.jsx
+++ workspace_react/frontend_shop/src/pages/BuyList.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import styles from './BuyList.module.css'
 import PageTitle from '../common/PageTitle'
 import axios from 'axios';
+import Button from '../common/Button'
+import Input from '../common/Input'
+import dayjs from 'dayjs';
 
 const BuyList = () => {
   //조회한 구매 목록 데이터를 저장할 state 변수
@@ -18,10 +21,58 @@ const BuyList = () => {
   }, [])
 
   return (
-    <div>
+    <div className={styles.container}>
       <PageTitle title='구매 이력 조회' />
-      <div></div>
-      <div></div>
+      <div className={styles.search_list}>
+        <div>
+          <span>주문번호</span>
+          <Input size='180px' />
+        </div>
+        <div>
+          <span>구매자ID</span>
+          <Input size='180px' />
+        </div>
+        <div>
+          <span>구매일시</span>
+          <Input type='date' />
+          <span>-</span>
+          <Input type={'date'} />
+        </div>
+        <div>
+          <Button title='검색' />
+        </div>
+      </div>
+      <div className={styles.buy_list}>
+        <p>총 x건이 검색되었습니다.</p>
+        <table>
+          <thead>
+            <tr>
+              <td>No</td>
+              <td>주문번호</td>
+              <td>구매상품</td>
+              <td>구매자ID</td>
+              <td>결제금액</td>
+              <td>구매일시</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              buyList.map((e, i) => {
+                return(
+                  <tr key={i}>
+                    <td>{buyList.length - i}</td>
+                    <td>{e.orderNum}</td>
+                    <td>{e.title}</td>
+                    <td>{e.memId}</td>
+                    <td>{e.price.toLocaleString() + '원'}</td>
+                    <td>{dayjs(e.buyDate).format('YYYY.MM.DD HH:mm')}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
